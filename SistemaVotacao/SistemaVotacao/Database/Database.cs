2f47afc26f5d829e5c956b1636a36fa59a356862
Vinicius <viniciusmsilva.ds@gmail.com>
2025-10-02 21:57:27 -0300

    Sistema de votação

---
+++ SistemaVotacao/SistemaVotacao/Database/Database.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SistemaVotacao.Database
+{
+    public class Database
+    {
+        private readonly string connectionString = "" +
+            "server=localhost;port=3306;" +
+            "database=sistemavoto_vinicius;" +
+            "user=root;" +
+            "password=12345678;";
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ SistemaVotacao/SistemaVotacao/Models/Votos.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaVotacao.Models
+{
+    public class Votos
+    {
+        public int id_voto { get; set; }
+        public int id_eleitor { get; set; }
+        public int id_candidato { get; set; }
+        public DateTime data_voto { get; set; }
+        public DateTime criado_em { get; set; }
+    }
+}
